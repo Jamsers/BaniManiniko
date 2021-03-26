61cd47ba5ee6b455332b02dc5ff78ba7d9987fcd
Jamsers <jjgutib@gmail.com>
2021-03-26 12:44:30 +0800

    [resolve #1]

---
+++ Source/BaniManiniko/BaniManiniko.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BaniManiniko.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, BaniManiniko, "BaniManiniko" );
---
+++ Source/BaniManiniko/BaniManinikoGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "BaniManinikoGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BANIMANINIKO_API ABaniManinikoGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
